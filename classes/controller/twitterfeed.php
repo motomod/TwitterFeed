0d1fcecb2d493c5be2b93d841c745676f5b4c52e
Matt De'Ath <matthewddddd@gmail.com>
2012-07-03 17:44:57 +0100

    Removed useless controller

--- classes/controller/twitterfeed.php
+++
@@ -1,9 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class Controller_TwitterFeed extends Controller {
-
-	public function action_view()
-	{
-		$this->response->body(Kostache::factory('twitterfeed'));
-	}
-}
